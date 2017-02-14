a3e8fb66d8383f04e8492815457d88a90d1fe844
Ísak Snær <sak Snr>
2017-02-14 10:32:04 +0000

    added uglify task to gruntfile and configured it to grunt minify

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -1,12 +1,31 @@
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-tslint');
+
 	var taskConfig = {
 		jshint: {
 			src:['src/app/**/*.js','!node_modules/**/*.js'],
 			options: {
-
-			}
+  				curly:  true,
+  				immed:  true,
+  				newcap: true,
+  				noarg:  true,
+  				sub:    true,
+  				boss:   true,
+  				eqnull: true,
+  				node:   true,
+  				undef:  true,
+  			globals: {
+    			_:       false,
+    			jQuery:  false,
+    			angular: false,
+    			moment:  false,
+    			console: false,
+    			$:       false,
+    			io:      false
+  				}
+ 			}
 		},
 
 		tslint: {
@@ -14,9 +33,17 @@ module.exports = function ( grunt ) {
 			options: {
 
 			}
+		},
+
+		uglify: {
+			build: {
+				src: ['src/app/**/*.ts'],
+				dest: ['build/min.ts']
+			}
 		} 
 	};
 	grunt.initConfig(taskConfig);
 
 	grunt.registerTask('default',['tslint']);
+	grunt.registerTask('minify', ['uglify']);
 }
